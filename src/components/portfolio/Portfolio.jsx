73818402c07900abae1f6644215386c76486dce2
zekud <zekudk@gmail.com>
2025-01-12 04:18:11 +0300

    Add Jara Booking Website to portfolio and update item IDs

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,37 +6,45 @@ import forum from "../../assets/forum.jpg";
 import booking from "../../assets/booking.jpg";
 import youtube from "../../assets/youtube.jpg";
 import solar from "../../assets/solar.jpg";
+import jara from "../../assets/jara.png";
 const items = [
   {
     id: 1,
+    title: "Jara Booking Website",
+    img: jara,
+    desc: "Fully responsive and dynamic booking website for Jarabeachresort with Paystack payment integration and an admin dashboard, featuring real-time booking management and seamless user experience.",
+    link: "https://booking.jarabeachresort.com/",
+  },
+  {
+    id: 2,
     title: "Zekua Chat",
     img: chat,
     desc: "Fully responsive and dynamic real-time chatting app powered by Firebase that allow users to share messages and images, with user-friendly features like email and Google authentication.",
     link: "https://zekua-chat.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Evangadi Forum",
     img: forum,
     desc: "Evangadi Forum is a Full Stack web application made with React,Tailwind CSS, Node.js, and MySQL that allow users to ask questions and give answer.",
     link: "https://evangadi-forum-client-git-main-zekuas-projects.vercel.app/login",
   },
   {
-    id: 3,
+    id: 4,
     title: "Little Lemon Booking",
     img: booking,
     desc: "The Little Lemon Online Reservation System is a Front end web application that allows users to reserve a table at a restaurant. ",
     link: "https://little-lemon-git-main-zekuas-projects.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Youtube To Mp3",
     img: youtube,
     desc: "Youtube To Mp3 is a web app that helps users to download their favorite videos in mp3 format. ",
     link: "https://youtube-to-mp-3-six.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Solar System",
     img: solar,
     desc: "Solar System is a web app that helps users to calculate the weight of any object in the solar system.",
